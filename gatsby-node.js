6f426f69038ea812e662c14688b523d14b909aee
Julio Mixco <julio.mixg@gmail.com>
2020-04-20 21:06:31 -0600

    move build files to docs

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,31 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require("path")
+const fs = require("fs")
+const buildFolder = "docs"
+// exports.onPreInit = () => {
+//   if (process.argv[2] === buildFolder) {
+//     fs.rmdirSync(path.join(__dirname, buildFolder), { recursive: true })
+//     if (fs.existsSync(path.join(__dirname, "public"))) {
+//       fs.renameSync(
+//         path.join(__dirname, "public"),
+//         path.join(__dirname, "public_dev")
+//       )
+//     }
+//   }
+// }
+
+exports.onPostBuild = () => {
+  fs.rmdirSync(path.join(__dirname, buildFolder), { recursive: true })
+  fs.renameSync(
+    path.join(__dirname, "public"),
+    path.join(__dirname, buildFolder)
+  )
+  //   if (fs.existsSync(path.join(__dirname, "public_dev"))) {
+  //     fs.renameSync(
+  //       path.join(__dirname, "public_dev"),
+  //       path.join(__dirname, "public")
+  //     )
+  //   }
+}
