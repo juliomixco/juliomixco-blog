e977423267c87f94cd5ad96a9d424b9a3af05d62
Julio Mixco <julio.mixg@gmail.com>
2020-04-22 09:42:02 -0600

    Updates

---
+++ component---src-pages-about-js-1bc385c36fe95b08b43b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"3XHS":function(t,e,n){"use strict";n.r(e),n.d(e,"query",(function(){return u}));n("q1tI");var o=n("oRs+"),a=n("qKvR");e.default=function(t){var e=t.data;return Object(a.b)(o.a,null,Object(a.b)("h1",null,"About ",e.site.siteMetadata.title),Object(a.b)("p",null,"We're the only site running on your computer dedicated to showing the best photos and videos of pandas eating lots of food."))};var u="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-about-js-1bc385c36fe95b08b43b.js.map
---
+++ component---src-templates-blog-post-js-88c18f37bab7bd1d7282.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{yZlL:function(t,n,e){"use strict";e.r(n),e.d(n,"query",(function(){return u}));e("q1tI");var r=e("oRs+"),a=e("qKvR");n.default=function(t){var n=t.data.markdownRemark;return Object(a.b)(r.a,null,Object(a.b)("div",null,Object(a.b)("h1",null,n.frontmatter.title),Object(a.b)("div",{dangerouslySetInnerHTML:{__html:n.html}})))};var u="861893041"}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-88c18f37bab7bd1d7282.js.map
