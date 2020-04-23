965fdab9e23e931db329952189fd5fe4e0362e66
Julio Mixco <julio.mixg@gmail.com>
2020-04-22 20:44:33 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-557e66d84d491ef54011.js"
+    "url": "webpack-runtime-3c447754383bfbf49b81.js"
   },
   {
     "url": "styles.3a535253d3b15c14acaf.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7673ffb1faef9c55848b42ff2eb35214"
+    "revision": "27345d04bc7932639ff5ddfdc8b026b3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4efef167a65f33edc4e5.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "264f9088b24b9535613b1ebf1a92723d"
+    "revision": "7b0bf49d557c55d170902765cc52b603"
   },
   {
     "url": "manifest.webmanifest",
