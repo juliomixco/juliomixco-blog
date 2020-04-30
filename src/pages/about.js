ee4318b4c64eb839228a29a9d8ea95a0e3585237
Julio Mixco <julio.mixg@gmail.com>
2020-04-30 12:19:29 -0600

    add email to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,12 +6,25 @@ export default ({ data }) => (
   <Layout>
     <h1>About {data.site.siteMetadata.title}</h1>
     <p>
-      🎉 Welcome to my Blog 👋. My name is Julio Mixco I'm a Software
-      Engineering Professional with 5+ years of experience in web development I
-      have worked as both backend and frontend developer with technologies like
-      TypeScript, Javascript, C#, Angular, React, NodeJS and .Net.
+      <span role="img" aria-label="tada">
+        🎉
+      </span>{" "}
+      Welcome to my Blog{" "}
+      <span role="img" aria-label="waving hand">
+        👋
+      </span>
+      . My name is Julio Mixco I'm a Software Engineering Professional with 5+
+      years of experience in web development I have worked as both backend and
+      frontend developer with technologies like TypeScript, Javascript, C#,
+      Angular, React, NodeJS and .Net.
     </p>
-    <p>I hope you enjoy this content</p>
+    <p>I hope you enjoy this content 🎈</p>
+
+    <p>
+      <a href="mailto:contact@juliomixco.com?subject=Business Oportunity">
+        contact@juliomixco.com
+      </a>
+    </p>
   </Layout>
 )
 
